aad426becd80bc742f9f653923170b62fd26fff0
arturo <arturogamez51@gmail.com>
2024-04-09 20:26:56 -0700

    setting the desktop mode for the login signup and forgot pass sections

--- client/app/[locale]/dashboard/(AuthSections)/login/page.js
+++ client/app/[locale]/dashboard/(AuthSections)/login/page.js
@@ -1,17 +1,25 @@
 import AuthForm from "@/app/components/AuthForm/authForm";
+import styles from "./page.module.css";
 
 import { login } from "@/lib/actions";
+import Symbols from "@/app/components/SVGIcons/symbols";
 
 // Languages
 const i18nNamespaces = ["authenticate"];
 
 export default async function SignupPage({ params }) {
     return (
-        <AuthForm
-            action={login}
-            type="login"
-            i18n={i18nNamespaces}
-            params={params}
-        />
+        <div className={styles.main}>
+            <AuthForm
+                action={login}
+                type="login"
+                i18n={i18nNamespaces}
+                params={params}
+            />
+            <div className={styles.splash}>
+                <Symbols type="reading-girl" size="400" />
+                <Symbols type="blob-1" size="550" className={styles.behind} />
+            </div>
+        </div>
     );
 }
--- client/app/[locale]/dashboard/(AuthSections)/signup/page.js
+++ client/app/[locale]/dashboard/(AuthSections)/signup/page.js
@@ -1,17 +1,25 @@
 import AuthForm from "@/app/components/AuthForm/authForm";
+import styles from "./page.module.css";
 
 import { signup } from "@/lib/actions";
+import Symbols from "@/app/components/SVGIcons/symbols";
 
 // Languages
 const i18nNamespaces = ["authenticate"];
 
 export default async function SignupPage({ params }) {
     return (
-        <AuthForm
-            action={signup}
-            i18n={i18nNamespaces}
-            type="signup"
-            params={params}
-        />
+        <div className={styles.main}>
+            <AuthForm
+                action={signup}
+                i18n={i18nNamespaces}
+                type="signup"
+                params={params}
+            />
+            <div className={styles.splash}>
+                <Symbols type="reading-girl" size="400" />
+                <Symbols type="blob-1" size="550" className={styles.behind} />
+            </div>
+        </div>
     );
 }
