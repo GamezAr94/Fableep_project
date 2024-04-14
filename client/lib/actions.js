3d5395039dea4ee40a06adf7addcd25541c687d6
arturo <arturogamez51@gmail.com>
2024-04-13 17:52:57 -0700

    adding the functionality of the login
    signup and login are ready we just need to show the feedback to the user

--- client/lib/actions.js
+++ client/lib/actions.js
@@ -5,7 +5,7 @@ import { cookies } from "next/headers";
 
 // function that will perform the signup process
 export async function signup(formData) {
-    // do some validdation for password and email
+    // TODO: do some validdation for password and email
     const rawData = {
         email: formData.get("email"),
         password: formData.get("password"),
@@ -28,11 +28,11 @@ export async function signup(formData) {
             // TODO display the errors in the client
             console.log(data.errors);
             return;
-        } else if (data.user && data.token) {
+        } else if (data.user && data.jwt_token) {
             // set the cookie to authorize the user
             cookies().set({
                 name: "token_auth",
-                value: data.token,
+                value: data.jwt_token,
                 secure: process.env.NODE_ENV === true,
                 httpOnly: true,
                 path: "/",
@@ -60,26 +60,57 @@ export async function signup(formData) {
 
 // function that will perform the login process
 export async function login(formData) {
-    // do some validdation for password and email
+    // TODO: do some validdation for password and email
     const rawData = {
         username: formData.get("email"),
         password: formData.get("password"),
     };
-    const res = await fetch(
-        `http://localhost:8080/${process.env.API}/${process.env.VERSION}/login`,
-        {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(rawData),
+
+    // send the request to the backend
+    try {
+        const res = await fetch(
+            `${process.env.IP}/${process.env.API}/${process.env.VERSION}/login`,
+            {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(rawData),
+            }
+        );
+        const data = await res.json();
+        if (data.errors) {
+            // TODO display the errors in the client
+            console.log(data.errors);
+            return;
+        } else if (data.user && data.jwt_token) {
+            // set the cookie to authorize the user
+            cookies().set({
+                name: "token_auth",
+                value: data.jwt_token,
+                secure: process.env.NODE_ENV === true,
+                httpOnly: true,
+                path: "/",
+                sameSite: "strict",
+                maxAge: process.env.JWT_EXPIRES_IN * 24 * 60 * 60,
+            });
+            console.log(data);
+        } else {
+            // TODO unexpected error
+            // if we are here then that means that there is an unhandled error
+            // we dont have errors and we dont have the user and token
+            console.log(data);
+            console.log("UNEXPECTED ERROR!!!");
+            return;
         }
-    );
-    const data = await res.json();
-    console.log(data);
-    if (data.success) {
-        redirect("/dashboard");
+    } catch (err) {
+        // there is a problem with the server
+        console.log(err.message);
+        return;
     }
+
+    // if we are here that means that the signup was successfully
+    redirect("/dashboard");
 }
 
 // function that will perform the login process
--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -7,6 +7,16 @@ const jwt = require("jsonwebtoken");
 const handleErrors = (err) => {
     let errors = { email: "", password: "" };
 
+    // incorrect email
+    if (err.message === "Incorrect email") {
+        errors.email = "That email is not registered";
+    }
+
+    // incorrect password
+    if (err.message === "Incorrect password") {
+        errors.email = "That password is incorrect";
+    }
+
     // duplicate error code
     if (err.code === 11000) {
         // TODO: set the i18n return an object maybe
@@ -39,13 +49,28 @@ exports.postSignup = async (req, res) => {
     try {
         const user = await User.create({ email, password });
         const token = createToken(user._id);
-        // TODO remove the log
-        console.log(token);
-        res.status(201).json({ user: user._id, token: token });
+        return res.status(201).json({ user: user._id, jwt_token: token });
     } catch (err) {
         const errors = handleErrors(err);
-        res.status(400).json({ errors });
+        return res.status(400).json({ errors });
     }
+};
+
+// Controller to login the user
+exports.postLogin = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.login(username, password);
+        if (user) {
+            const token = createToken(user._id);
+            return res.status(200).json({ user: user._id, jwt_token: token });
+        }
+    } catch (err) {
+        const errors = handleErrors(err);
+        return res.status(400).json({ errors: errors });
+    }
+    return res.status(201).json({});
 };
 
 // Controller to fetch all the users
@@ -53,25 +78,4 @@ exports.getUsers = (req, res) => {
     const users = User.fetchAll();
     console.log(users);
     return res.status(201).json(users);
-};
-
-// Controller to login the user
-exports.postLogin = async (req, res) => {
-    /*
-    const { username, password } = req.body;
-
-    if (!username || !password) {
-        return res.status(400).send("Username and password are required");
-    }
-
-    const user = new User(username, password);
-
-    const result = user.login();
-
-    if (!result.success) {
-        return res.status(400).json(result);
-    }
-
-    return res.status(201).json(result);
-    */
 };
