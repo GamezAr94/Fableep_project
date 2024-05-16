1d27ed94dc35912bdab58ef9226a42f6f777b40c
arturo <arturogamez51@gmail.com>
2024-05-15 21:34:39 -0700

    finishing the layout for the home page
    now it is responsive in phone tablet and desktop

--- client/app/[locale]/(public)/page.js
+++ client/app/[locale]/(public)/page.js
@@ -1,5 +1,28 @@
 import styles from "./page.module.css";
 
 export default function Home() {
-    return <main>hola</main>;
+    return (
+        <div className={styles.main_container}>
+            <div className={styles.description_section}>
+                <main>
+                    <div className={styles.header_container}>
+                        <h1>AI Story Generator</h1>
+                        <h2>Create BEAUTIFUL bedtime stories for your kids.</h2>
+                    </div>
+                    <h3>
+                        create good habits and spend quality time with your
+                        loved ones.
+                    </h3>
+                    <div>desc</div>
+                </main>
+            </div>
+            <div className={styles.imagine_section}>
+                <div className={styles.image_banner}>img</div>
+            </div>
+            <div className={styles.phone_container}>
+                <div className={styles.phone_img}>phone</div>
+            </div>
+            <div className={styles.news_container}>news</div>
+        </div>
+    );
 }
