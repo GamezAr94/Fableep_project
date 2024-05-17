d6a3687b02a1fde242fab90923a0735862256add
arturo <arturogamez51@gmail.com>
2024-05-16 23:32:04 -0700

    finishing the first step in the banner img style

--- client/app/components/homePageSections/bannerSection.js
+++ client/app/components/homePageSections/bannerSection.js
@@ -1,9 +1,18 @@
+import Image from "next/image";
 import styles from "./bannerSection.module.css";
 
 export default function BannerSection() {
     return (
         <div className={styles.imagine_section}>
-            <div className={styles.image_banner}>img</div>
+            <div className={styles.image_banner}>
+                <Image
+                    src="/images/general/bear_v2.png"
+                    alt="A majestic bear"
+                    width={1456} // Adjust for your image's actual size
+                    height={816}
+                    priority={true}></Image>
+                <div className={styles.rounded_mask_first}></div>
+            </div>
         </div>
     );
 }
