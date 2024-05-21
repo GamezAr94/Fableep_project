6649be56da58d866c9b5dc43b0a2eb6e9a516c1a
arturo <arturogamez51@gmail.com>
2024-05-20 19:00:28 -0700

    adding the news section carousel
    it is not 100% finished we need to add content and images and maybe a title and an action button

--- client/app/components/homePageSections/newsSection.js
+++ client/app/components/homePageSections/newsSection.js
@@ -1,5 +1,59 @@
+"use client";
+import Slider from "react-slick";
 import styles from "./newsSection.module.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 export default function NewsSection() {
-    return <div className={styles.news_container}>news</div>;
+    var settings = {
+        dots: true,
+        infinite: true,
+        speed: 1000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        cssEase: "ease-out",
+        autoplaySpeed: 4000,
+        nextArrow: <p arrow="true"></p>,
+        prevArrow: <p arrow="true"></p>,
+    };
+    return (
+        <div className={styles.news_container}>
+            <Slider className={styles.slider_comp} {...settings}>
+                <div className={styles.slide_container}>
+                    <div className={styles.text_part}>
+                        <p>
+                            Unleash the creativity and curiosity by fostering a
+                            love for the books.
+                        </p>
+                    </div>
+                    <div className={styles.image_part}>
+                        <Image
+                            src="/images/general/bear_v2.png"
+                            alt="A cute bear sleeping among books in a clear and tranquile night"
+                            width={1456} // Adjust for your image's actual size
+                            height={816}
+                            priority={true}></Image>
+                    </div>
+                </div>
+                <div className={styles.slide_container}>
+                    <div className={styles.text_part}>
+                        <p>
+                            Unleash the creativity and curiosity by fostering a
+                            love for the books.
+                        </p>
+                    </div>
+                    <div className={styles.image_part}>
+                        <Image
+                            src="/images/general/bear_v2.png"
+                            alt="A cute bear sleeping among books in a clear and tranquile night"
+                            width={1456} // Adjust for your image's actual size
+                            height={816}
+                            priority={true}></Image>
+                    </div>
+                </div>
+            </Slider>
+        </div>
+    );
 }
